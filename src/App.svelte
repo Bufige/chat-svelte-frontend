bdad65147808784c84b893accd6dc550ee17758a
Leonardo igor <bufige@yahoo.com>
2020-10-07 21:27:07 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,111 @@
+<script>
+	import { onMount, onDestroy } from "svelte";
+	import io from "socket.io-client";
+
+	import Message from "./Message.svelte";
+
+
+	const socket = io("https://bufige-chat-server.herokuapp.com:3000");
+
+	let inputUser;
+	let inputMessage;
+
+	let user = "";
+	let inputUserValue;
+	let messages = [];
+	let message = "";
+
+	socket.on("message", (data) => {
+		messages = [...messages, data];
+	});
+
+	onMount( () => {
+		inputUser.focus();
+	})
+	onDestroy(async () => {
+		socket.disconnect();
+	});
+
+	const sendMessage = () => {
+		if (message) {
+			socket.emit("add-message", generateMessage(message));
+		}
+
+		message = null;
+	};
+	const selectUsername = () => {
+		user = inputUserValue;
+		
+		setTimeout(() => {
+			inputMessage.focus();
+		}, 500);
+		
+	}
+	const generateMessage = (msg) => {
+		return {
+			content: msg,
+			time: new Date().toLocaleTimeString(),
+			username: user
+		};
+	};
+</script>
+
+<style>
+	.content {
+		height: calc(100% - 45px);
+		overflow-y: scroll;
+	}
+	form {
+		position: relative;
+		width: 100%;
+	}
+
+	form > input {
+		width: 100%;
+
+		padding-left: 10px;
+		padding-right: 60px;
+		border-radius: 20px;
+
+		user-select: none;
+		outline: none;
+	}
+
+	form > button {
+		position: absolute;
+		right: 0;
+		border-radius: 20px;
+		border: 1px solid grey;
+	}
+
+	.select {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		font-size: 18px;
+	}
+</style>
+
+{#if !user}
+	<div class="select">
+		Select a username
+		<form on:submit|preventDefault={selectUsername}>
+			<input bind:this={inputUser} bind:value={inputUserValue} placeholder='your username'/>
+			<button type='submit'>Select</button>
+		</form>
+	</div>
+{:else}
+	<div class="content">
+		{#each messages as msg}
+			<Message message={msg} />
+		{/each}
+	</div>
+
+	<form on:submit|preventDefault={sendMessage}>
+		<input bind:this={inputMessage} bind:value={message} placeholder='your message'/>
+		<button type="submit">Send</button>
+	</form>
+{/if}
---
+++ src/Message.svelte
@@ -0,0 +1,54 @@
+<script>
+	export let message;
+
+	console.log(message);
+</script>
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+	.container {
+		width: calc(100% - 4px);
+		margin: 0 2px;
+		border: 1px solid grey;
+		border-radius: 20px;
+
+		user-select: none;
+		word-wrap: break-word;
+
+		margin-bottom: 3px;
+	}
+
+	.time {
+		font-size: 10px;
+		padding: 5px 10px;
+	}
+	.content {
+		width: calc(100% - 10px);
+		margin: 0 10px;
+	}
+
+	.username {
+		font-size: 22px;
+	}
+	.message {
+		display: block;
+		padding-right: 10px;
+	}
+</style>
+
+<div class="container">
+	<div class="content">
+		<div class="username">
+			{message.username}
+		</div>
+		<div class="message">
+			{message.content}
+		</div>
+	</div>	
+	<div class="time">
+		{message.time}
+	</div>
+</div>
